9e8a7bd61f8a2a8fb9695d980b4eb6df6d4c837f
Stefan Glüge <glue@zhaw.ch>
2025-09-25 08:37:43 +0000

    remove commented-out plot_log_prower_spec function call from load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -232,5 +232,4 @@ print(plot_title)
 # plot_two_channel_spectrogram(transformed_data.cpu().numpy(), title=plot_title)
 # plot_input_data(spectrogram_2d=transformed_data.cpu().numpy(),
 #                 iq_2d=iq_data.cpu().numpy(), title=plot_title)
-# plot_log_prower_spec(spectrogram_2d=transformed_data.cpu().numpy(), n_fft=1024, sampling_rate=14e6, title=plot_title)
 plot_input_data_powerspec(spectrogram_2d=transformed_data.cpu().numpy(), iq_2d=iq_data.cpu().numpy(), title=plot_title)
